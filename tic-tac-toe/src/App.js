660e374d58bea367634acf018aabb7fc80751c68
Quinten Krauth <quintenkrauth19@gmail.com>
2022-11-11 13:00:27 -0500

    working tic tac toe!

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,18 +1,58 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Square from "./Square";
 
 function App() {
-  // const firstVariable = "This is my string of choice";
-
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
 
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
+
+  const calculateWinner = (array) => {
+    const lines = [
+      [0, 1, 2],
+      [0, 3, 6],
+      [0, 4, 8],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (array[a] && array[a] === array[b] && array[a] === array[c]) {
+        return `${array[a]} wins!`;
+      }
+    }
+    return "Who will win?";
+  };
+
   return (
     <div className="App">
-      <Square squares={squares} setSquares={setSquares} player={player} setPlayer={setPlayer}/>
+      <span>{calculateWinner(squares)}</span>
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+              squareValue={value}
+              index={index}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
   );
-};
+}
 
 export default App;
